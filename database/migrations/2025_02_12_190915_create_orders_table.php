67552320fa137b52f476b5a696a081883381cde8
FlorianKoning <florian.koning2004@gmail.com>
2025-02-16 00:33:13 +0100

    Updated orders tabel in database.

--- database/migrations/2025_02_12_190915_create_orders_table.php
+++ database/migrations/2025_02_12_190915_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->json('order_items');
             $table->foreign('user_id')->references('id')->on('users');
             $table->decimal('payment_amount');
             $table->unsignedBigInteger('order_status_id');
