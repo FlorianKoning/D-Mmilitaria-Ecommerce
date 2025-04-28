d80a10006ea0faa82e1cab357dc8660c1e7af11e
Florian Koning <florian.koning2004@gmail.com>
2025-04-28 13:49:11 +0200

    Orders page height fixed for when user has no orders.

--- resources/views/profile/orders.blade.php
+++ resources/views/profile/orders.blade.php
@@ -8,7 +8,7 @@
     {{-- profile navbar --}}
     <x-profile-navbar />
 
-    <div class="py-12">
+    <div class="py-12 h-screen">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-full">
